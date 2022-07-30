b26e1e056c74765c48cb27c4ab97a7ac2fc7350a
Martin Balao <m@rtin.uy>
2022-07-30 03:35:26 -0400

    Fix in syscalls wrapper for x86_64.

--- src/module/simplemodule_main.c
+++ src/module/simplemodule_main.c
@@ -249,15 +249,21 @@ static long run_module_test(unsigned long arg) {
             while (args_i < args_count)
                 syscall_args[args_i++] = *(data_ptr++);
             args_i = 0x0UL;
+            #if LINUX_VERSION_CODE <= KERNEL_VERSION(5,0,0)
             while (args_i < args_count) {
-                #if LINUX_VERSION_CODE <= KERNEL_VERSION(5,0,0)
                 syscall_set_arguments(current, &regs, (unsigned int)args_i++,
                         (unsigned int)args_count, syscall_args);
-                #else // LINUX_VERSION_CODE
-                memcpy(&(regs.bx) + (unsigned int)args_i++, syscall_args,
-                        (unsigned int)args_count * sizeof(syscall_args[0]));
-                #endif // LINUX_VERSION_CODE
             }
+            #else // LINUX_VERSION_CODE
+            {
+                regs.di = syscall_args[args_i++];
+                regs.si = syscall_args[args_i++];
+                regs.dx = syscall_args[args_i++];
+                regs.r10 = syscall_args[args_i++];
+                regs.r8 = syscall_args[args_i++];
+                regs.r9 = syscall_args[args_i];
+            }
+            #endif // LINUX_VERSION_CODE
             preempt_disable();
             if (syscall_number == __NR_getuid) {
                 GDB("print ((struct task_struct*)(0x%px))->pid", current);
