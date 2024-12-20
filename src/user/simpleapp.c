277758871f831fe1195b527bd8324a1a7361d537
Francisco Ferrari Bihurriet <fferrari@redhat.com>
2024-12-20 21:12:01 +0100

    Print bpf_log_buf even if the BPF prog failed

--- src/user/simpleapp.c
+++ src/user/simpleapp.c
@@ -152,9 +152,10 @@ static int test_sock(void)
                tcp_cnt, udp_cnt, icmp_cnt);
         sleep(1);
     }
-    SA_LOG(MIN_VERBOSITY, "%s\n", bpf_log_buf);
 
 cleanup:
+    SA_LOG(MIN_VERBOSITY, "%s\n", bpf_log_buf);
+
     /* maps, programs, raw sockets will auto cleanup on process exit */
     return 0;
 }
